6960f7711f6b71b08b7fbb579573005ad4952fea
oldglass <seosangwon0419@gmail.com>
2023-06-12 23:15:45 +0900

    js 롤백

--- src/main/resources/static/js/parking.js
+++ src/main/resources/static/js/parking.js
@@ -50,13 +50,19 @@ function responseAjax() {
     })
 }
 
-/* html에서 json파일을 javascript형태로 불러올 때 사용한다.
 function changeFromFile() {
     for (let i = 0; i < blanks.length; i++) {
         change_parking_state(i, blanks[i]);
     }
-} */
 
+    let blanks_count = blanks.filter(element => 0 === element).length;
+    let disabled_blanks_count = blanks.slice(21, 23).filter(element => 0 === element).length;
+
+    parking_blanks_count.innerText = `빈 주차 자리 개수: ${blanks_count - disabled_blanks_count}개`;
+    parking_disabled_blanks_count.innerText = `빈 장애인 주차 자리 개수: ${disabled_blanks_count}개`;
+}
+
+/* fetch() 함수로 json파일을 불러온다.
 function changeFromFile() {
     fetch("../json/result.json")
     .then((response) => response.json())
@@ -73,7 +79,7 @@ function changeFromFile() {
         parking_blanks_count.innerText = `빈 주차 자리 개수: ${blanks_count - disabled_blanks_count}개`;
         parking_disabled_blanks_count.innerText = `빈 장애인 주차 자리 개수: ${disabled_blanks_count}개`;
     })
-}
+} */
 
 const parking_lots = document.getElementsByClassName("parking_lots");
 const parking_disabled_lots = document.getElementsByClassName("parking_disabled_lots");
@@ -84,11 +90,11 @@ const resUrl = "/parking/update";
 const color_red = "#FF3636";
 const color_green = "#6dcc60";
 
-changeFromFile();
 
-setInterval(() => {
+
+/* setInterval(() => {
     changeFromFile();
-}, 5000);
+}, 5000); */
 
 /* ajax로 DTO에 값을 보낼 때 사용한다. 
 const blank_list = {"parkingInfo": [1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 0]};  // 전송 테스트 용
@@ -96,12 +102,13 @@ let blanks = [];
 responseAjax(); // 전송 테스트
 requestAjax(); */
 
-/* html에서 json파일을 javascript형태로 불러올 때 사용한다.
 let data = JSON.parse(JSON.stringify(Parking));
 let blanks = data.parkingInfo.slice(1);
+
+changeFromFile();
+
 setInterval(() => {
     data = JSON.parse(JSON.stringify(Parking));
     blanks = data.parkingInfo.slice(1);
     changeFromFile();
-    console.log("running...");
-}, 10000); */
+}, 10000);
