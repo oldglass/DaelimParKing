be7be338d9c5a6999f1629eae6b2e761c9a91a5b
oldglass <seosangwon0419@gmail.com>
2023-06-12 21:54:23 +0900

    json파일 불러오기 변경
    fetch() 사용

--- src/main/resources/static/js/parking.js
+++ src/main/resources/static/js/parking.js
@@ -50,10 +50,29 @@ function responseAjax() {
     })
 }
 
+/* html에서 json파일을 javascript형태로 불러올 때 사용한다.
 function changeFromFile() {
     for (let i = 0; i < blanks.length; i++) {
         change_parking_state(i, blanks[i]);
     }
+} */
+
+function changeFromFile() {
+    fetch("../json/result.json")
+    .then((response) => response.json())
+    .then((json) => {
+        let blanks = json.parkingInfo.slice(1);
+        
+        for (let i = 0; i < blanks.length; i++) {
+            change_parking_state(i, blanks[i]);
+        }
+
+        let blanks_count = blanks.filter(element => 0 === element).length;
+        let disabled_blanks_count = blanks.slice(21, 23).filter(element => 0 === element).length;
+
+        parking_blanks_count.innerText = `빈 주차 자리 개수: ${blanks_count - disabled_blanks_count}개`;
+        parking_disabled_blanks_count.innerText = `빈 장애인 주차 자리 개수: ${disabled_blanks_count}개`;
+    })
 }
 
 const parking_lots = document.getElementsByClassName("parking_lots");
@@ -65,29 +84,24 @@ const resUrl = "/parking/update";
 const color_red = "#FF3636";
 const color_green = "#6dcc60";
 
-
-// const blank_list = {"parkingInfo": [1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 0]};  // 전송 테스트 용
-
-// let blanks = [];
-// responseAjax(); // 전송 테스트
-// requestAjax();
-
-// setInterval(() => {
-//     requestAjax();    
-// }, 60000);   
-
-let data = JSON.parse(JSON.stringify(Parking));
-let blanks = data.parkingInfo.slice(1);
 changeFromFile();
 
+setInterval(() => {
+    changeFromFile();
+}, 10000);
+
+/* ajax로 DTO에 값을 보낼 때 사용한다. 
+const blank_list = {"parkingInfo": [1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 0]};  // 전송 테스트 용
+let blanks = [];
+responseAjax(); // 전송 테스트
+requestAjax(); */
+
+/* html에서 json파일을 javascript형태로 불러올 때 사용한다.
+let data = JSON.parse(JSON.stringify(Parking));
+let blanks = data.parkingInfo.slice(1);
 setInterval(() => {
     data = JSON.parse(JSON.stringify(Parking));
     blanks = data.parkingInfo.slice(1);
     changeFromFile();
-}, 3000);
-
-let blanks_count = blanks.filter(element => 0 === element).length;
-let disabled_blanks_count = blanks.slice(21, 23).filter(element => 0 === element).length;
-
-parking_blanks_count.innerText += ` ${blanks_count - disabled_blanks_count}개`;
-parking_disabled_blanks_count.innerText += ` ${disabled_blanks_count}개`;
+    console.log("running...");
+}, 10000); */
